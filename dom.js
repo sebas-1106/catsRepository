bcbfd63720a27a2f838841b4e3b862c0688621ed
Sebastian_Acevedo <acevedosebastian455@gmail.com>
2024-11-13 12:26:38 +0100

    segundo commit

---
+++ dom.js
@@ -0,0 +1,39 @@
+let body= document.getElementsByTagName("body");
+let main= document.getElementsByTagName("main");
+let ol= document.getElementsByTagName("ol");
+let ul= document.getElementsByTagName("ul");
+let li= document.getElementsByTagName("li");
+let hTres=document.getElementsByTagName("h3");
+let a= document.getElementsByTagName("a");
+let img=document.getElementsByTagName("img");
+let mainHeader=document.getElementById("mainHeader");
+let thunder=document.getElementsByClassName("thunder");
+
+console.log("Body: ",body );
+console.log("mainHeader: ",mainHeader);
+console.log("thunder: ", thunder);
+// console.log("body hijos: ", body[0].childNodes);
+// body[0].style = "background-color: lightcyan";
+body[0].style.backgroundColor = "lightcyan";
+main[0].style.paddingTop="1rem";
+main[0].style.paddingLeft="1rem";
+main[0].style.color="Black";
+
+main[0].style.paddingTop="1rem";
+a[0].style.color="plum";
+a[0].style.fontStyle="italic";
+ol[0].style.paddingLeft="2rem";
+ul[0].style.paddingLeft="2rem";
+li[0].style.color="crimson";
+thunder[0].style.color="green";
+h3[0].style.fontStyle="italic";
+img[0].style.borderRadius="1rem";
+img[0].style.width="400px";
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,39 @@
+// alert("hola mundo")
+let product= "bandeja paisa";
+//declaracion variable en linea
+let price= "50.000", dimensions=0;
+//declaracion varias variables en una misma linea
+product="paisa bandeja";
+alert(product);
+// let return=true; ->no se pueden declarar variables con palabras reservadas
+let pokemon="charizard";
+
+function showPokemon(nombrePokemon) {
+    
+    pokemon="charizard";
+    console.log(nombrePokemon);
+}
+showPokemon(pokemon);
+
+let firstName="John";
+let lastName="Snow";
+let age=24;
+console.log("soy "+firstName + " "+ lastName+ " tengo "+ age+ " años ");
+// gameOfTrhonesPresentacion(firstName, lastName, age){
+
+
+age += 6;
+console.log("soy "+firstName + " "+ lastName+ " tengo "+ age+ " años ");
+age *= 6;
+console.log("soy "+firstName + " "+ lastName+ " tengo "+ age+ " años ");
+age /=2;
+console.log("soy "+firstName + " "+ lastName+ " tengo "+ age+ " años ");
+age ++;
+console.log("soy "+firstName + " "+ lastName+ " tengo "+ age+ " años ");
+
+age +=25;
+console.log("soy "+firstName + " "+ lastName+ " tengo "+ age+ " años ");
+age /=5;
+console.log("soy "+firstName + " "+ lastName+ " tengo "+ age+ " años ");
+
+
